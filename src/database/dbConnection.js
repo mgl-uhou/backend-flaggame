8d274a7fdae9de47b4a0e2afa2990e1c6ae5f17f
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-12-04 17:04:58 -0300

    feat: atualizado para funcionar com o supabase

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -4,12 +4,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "dbflaggame",   
-  process.env.DB_USER || "root",    
+  process.env.DB_NAME || "",   
+  process.env.DB_USER || "",    
   process.env.DB_PASS || "",    
   {
-    host: process.env.DB_HOST || "localhost",
-    dialect: process.env.DB_DIALECT || "mysql",
+    host: process.env.DB_HOST || "",
+    dialect: process.env.DB_DIALECT || "postgres",
     logging: false, // Desabilita logs de queries
   }
 );
