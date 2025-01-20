c949050754be062dc977d5926f322943b4509e11
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-01-20 13:25:12 -0300

    test: starting tests

---
+++ src/controllers/tests/UserController.spec.js
@@ -0,0 +1,9 @@
+// DESCRIBE -> bloco de testes - tests suites
+// IT or TEST -> declara um único teste unitário - tests cases
+// EXPECT -> asserções dos resultados - validar resultados
+
+describe("Initial tests", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	})
+})
