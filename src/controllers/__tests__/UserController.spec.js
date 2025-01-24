8e070d0246fc1ec0c7a7c070781b84d04dae65f4
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-01-24 12:48:24 -0300

    feat: criando testes de testes

--- src/controllers/__tests__/UserController.spec.js
+++ src/controllers/__tests__/UserController.spec.js
@@ -2,8 +2,16 @@
 // IT or TEST -> declara um único teste unitário - tests cases
 // EXPECT -> asserções dos resultados - validar resultados
 
+function sum(a, b) {
+	return a + b;
+}
+
 describe("Initial tests", () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
+	});
+
+	test("First unit test", () => {
+		expect(sum(1, 2)).toBe(3);
 	})
 })
